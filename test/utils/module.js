7216f1c53f7b8a5ab78dd236bee7ec66c47f72b8
Irakli Gozalishvili <rfobic@gmail.com>
2010-11-01 04:17:37 +0100

    Adding more module tests and updateing existing ones.

--- test/utils/module.js
+++ test/utils/module.js
@@ -1,35 +1,122 @@
 'use strict'
 
-var ID = require('teleport/utils/module').ID
-,   npm = require('teleport/strings').NPM_DIR
+var Module = require('teleport/catalog/module').Module
+,   npmDir = require('../fixtures').npmDir
 
-exports['test normal module'] = function(assert) {
-  var id = ID('teleport/engine/xhr+eval.js')
-  assert.equal(id.packageName, 'teleport', 'package name is a first slice')
-  assert.equal(id.version, 'active', 'verision is supposed to be `active`')
-  assert.equal(id.isMain, false, 'module is not main')
-  assert.equal(id.modulePath, 'engine/xhr+eval', 'module path is correct')
-  assert.equal(id.topId, 'teleport/engine/xhr+eval', 'module id is correct')
-  assert.equal
-  ( id.path
-  , npm + '/teleport/active/package/lib/engine/xhr+eval.js'
-  , 'module path is generated correctly'
+exports.Assert = require('../asserts').Assert
+
+exports['test regular module'] = function(assert) {
+  assert.module
+  ( Module(
+    { url: 'bar/module/xhr+eval.js'
+    , packages: { bar: { name: 'bar' } }
+    , packagesPath: npmDir
+    })
+  , { dir: npmDir
+    , name: 'bar'
+    , id: 'bar/module/xhr+eval'
+    , path: './lib/module/xhr+eval.js'
+    , relative: 'module/xhr+eval'
+    }
+  , 'test module `bar/module/xhr+eval.js`'
   )
 }
 
 exports['test main module'] = function(assert) {
-  var id = ID('teleport')
-  assert.equal(id.packageName, 'teleport', 'package name is a first slice')
-  assert.equal(id.version, 'active', 'verision is supposed to be `active`')
-  assert.equal(id.isMain, true, 'module is not main')
-  assert.equal(id.modulePath, 'teleport', 'module path is correct')
-  assert.equal(id.topId, 'teleport', 'module id is correct')
-  assert.equal
-  ( id.path
-  , npm + '/teleport/active/package/lib/teleport.js'
-  , 'module path is generated correctly'
+  assert.module
+  ( Module(
+    { url: 'bar.js'
+    , packages: { 'bar': { name: 'bar', main: './lib/foo' } }
+    , packagesPath: npmDir
+    })
+  , { dir: npmDir
+    , id: 'bar'
+    , main: true
+    , name: 'bar'
+    , path: './lib/foo.js'
+    }
+  , 'test module with main property in descriptor'
+  )
+
+  assert.module
+  ( Module(
+    { url: 'foo.js'
+    , packages: { 'foo': { name: 'foo', main: './path/to/bar.js' } }
+    , packagesPath: npmDir
+    })
+  , { dir: npmDir
+    , id: 'foo'
+    , name: 'foo'
+    , main: true
+    , path: './path/to/bar.js'
+    }
+  , 'module with main property pointing outside of lib dir.'
   )
 }
 
+exports['test versioned package'] = function(assert) {
+  assert.module
+  ( Module(
+    { url: 'bar@0.1.1/baz.js'
+    , packages: { bar: { name: 'bar' } }
+    , packagesPath: npmDir
+    })
+  , { dir: npmDir
+    , name: 'bar'
+    , version: '0.1.1'
+    , id: 'bar@0.1.1/baz'
+    , path: './lib/baz.js'
+    }
+  , 'module with versioned package name'
+  )
+}
+
+exports['test custom lib'] = function(assert) {
+  assert.module
+  ( Module(
+    { url: 'bar/test.js'
+    , packages: { bar: { name: 'bar', directories: { lib: 'engine/teleport' } } }
+    , packagesPath: npmDir
+    })
+  , { dir: npmDir
+    , name: 'bar'
+    , id: 'bar/test'
+    , path: './engine/teleport/test.js'
+    }
+  , 'module with custom lib path'
+  )
+}
+
+exports['test module alias'] = function(assert) {
+  var packages = { bar: { name: 'bar', modules: { foo: './path/to/foo.js' } } }
+
+  assert.module
+  ( Module(
+    { url: 'bar/test.js'
+    , packages: packages
+    , packagesPath: npmDir
+    })
+  , { dir: npmDir
+    , name: 'bar'
+    , id: 'bar/test'
+    , path: './lib/test.js'
+    }
+  , 'non aliased module form package with aliases'
+  )
+
+  assert.module
+  ( Module(
+    { url: 'bar/foo.js'
+    , packages: packages
+    , packagesPath: npmDir
+    })
+  , { dir: npmDir
+    , name: 'bar'
+    , id: 'bar/foo'
+    , path: './path/to/foo.js'
+    }
+  , 'aliased module'
+  )
+}
 
 if (module == require.main) require('test').run(exports)
