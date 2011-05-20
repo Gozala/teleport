24df7bca125401e8b15de639578b92a27d378183
Irakli Gozalishvili <rfobic@gmail.com>
2011-05-20 18:15:49 +0200

    Updating to npm 1.x.x

---
+++ app.js
@@ -0,0 +1,95 @@
+/* vim:set ts=2 sw=2 sts=2 expandtab */
+/*jshint asi: true undef: true es5: true node: true devel: true
+         forin: true latedef: false  globalstrict: true */
+/*global define: true */
+
+"use strict";
+
+var path = require('path')
+var modules = require('module')
+var npm = require('npm')
+var readInstalled = require('npm/lib/utils/read-installed')
+var fs = require('fs')
+
+
+/**
+ * This function takes json data returned by npm's `readInstalled` and cleans
+ * it up from circular references so that `JSON.stringify` can be called on it.
+ */
+function Metadata(data) {
+  delete data.parent;
+  if (data.dependencies) {
+    Object.keys(data.dependencies).forEach(function(name) {
+      Metadata(data.dependencies[name]);
+    });
+  }
+  return data;
+}
+exports.Metadata = Metadata
+
+function Module(id, parent) {
+  this.parent = parent
+  this.id = id
+  this.filename = modules._resolveFilename(id, parent)[0]
+  this.paths = modules._nodeModulePaths(path.dirname(this.filename))
+}
+Module.prototype.resolve = function resolve(id) {
+  return new Module(id, this)
+}
+Module.prototype.toString = function toString() {
+  return fs.readFileSync(this.filename).toString()
+}
+exports.Module = Module
+
+function Teleport(data) {
+  this.metadata = new Metadata(data)
+  this.dashboard = path.resolve(module.id, '../node_modules/teleport-dashboard')
+  this.module = Object.create(Module.prototype, {
+    parent: { value: null },
+    id: { value: '/' },
+    filename: { value: '.' },
+    paths: {
+      value: [
+        data.path,
+        path.join(data.path, 'node_modules'),
+        this.dashboard,
+        path.join(this.dashboard, 'node_modules'),
+      ]
+    }
+  })
+  console.log(this.module.paths)
+}
+exports.Teleport = Teleport
+Teleport.prototype.router = function router(path) {
+  path = path || '/support'
+  var teleport = this
+  return function router(app) {
+    app.get(path + '/metadata.json', function onMeta(request, response, next) {
+      next(JSON.stringify(teleport.metadata, '', '  '))
+    })
+    app.get(path + '/*.js', function onModule(request, response, next) {
+      var id = request.params[0]
+      try {
+        var source = '!define(function(require, exports, module, undefined, define) { ' +
+                     teleport.resolve(id).toString() +
+                     '\n/**/});'
+          response.end(source)
+      } catch (error) {
+        next(JSON.stringify(error))
+      }
+    })
+  }
+}
+Teleport.prototype.resolve = function resolve(id, base) {
+  return this.module.resolve(id)
+}
+
+exports.teleport = function teleport(path, callback) {
+  npm.load(function onLoad(error) {
+    if (error) return callback(error)
+    readInstalled(path,  function onRead(error, data) {
+      if (error) return callback(error)
+      callback(error, new Teleport(data))
+    })
+  })
+}
--- service.js
+++ service.js
@@ -1,187 +1,23 @@
-'use strict'
-
-var http = require('http')
-,   mimeType = require('mime').lookup
-,   fs =  require('promised-fs')
-,   when = require('q').when
-,   all = require('promised-utils').all
-,   Registry = require('./registry').Registry
-,   Promised = require('promised-utils').Promised
-,   getDescriptor = require('./project').getDescriptor
-,   CONST = require('./strings')
-,   parseURL = require('url').parse
-
-,   server = http.createServer()
-,   lib = fs.Path(module.filename).directory().directory()
-,   core = lib.join(CONST.TELEPORT_CORE_FILE).read()
-,   engine = lib.join(CONST.ENGINES_DIR, CONST.TELEPORT_ENGINE_FILE).read()
-,   deprecatedPath = 'packages/teleport.js'
-,   newTeleportPath = 'support/teleport/teleport.js'
-,   DEP_DIR = '/support/'
-
-var registry = Registry();
-var playground = registry.invoke('packages.teleport-dashboard.getContent'
-                                , ['resources/pages/404.html'])
+/* vim:set ts=2 sw=2 sts=2 expandtab */
+/*jshint asi: true undef: true es5: true node: true devel: true
+         forin: true latedef: false supernew: true globalstrict: true */
+/*global define: true */
 
 
-function isUnderPackages(path) {
-  var index = path.indexOf('support/')
-  return index >= 0 && index <= 1 && 10 < path.length
-}
+'use strict';
 
-function redirectTo(url, response) {
-  response.writeHead(302, { 'Location': url })
-  response.end()
-}
-
-function makePackageRedirectURL(name, url) {
-  var redirectURL = DEP_DIR + name
-  if (url !== '/support' && url !== DEP_DIR) redirectURL += url
-  return redirectURL
-}
-
-function isPathToFile(path) {
-  return 0 <= String(path).substr(path.lastIndexOf('/') + 1).indexOf('.')
-}
-function compeletPath(path) {
-  path = String(path)
-  if (!isPathToFile(path)) {
-    if ('/' !== path.charAt(path.length - 1)) path += '/'
-    path += 'index.html'
-  }
-  return path
-}
-function normalizePath(path) {
-  if (!isPathToFile(path) && '/' !== path.charAt(path.length - 1)) path += '/'
-  return path
-}
-
-function stripOutDependenciesPath(path) {
-  return 0 == path.indexOf(DEP_DIR) ? path.substr(DEP_DIR.length) : path
-
-}
-function getPackageName(path) {
-  path = stripOutDependenciesPath(String(path))
-  var parts = path.split('/')
-  return parts[0] || parts[1]
-}
-
-function getPackageRelativePath(path, name) {
-  path = stripOutDependenciesPath(String(path))
-  if (name) path = path.substr(path.indexOf(name) + name.length)
-  return path
-}
-
-function isJSPath(path) {
-  return '.js' === String(path).substr(-3)
-}
-function removeJSExtension(path) {
-  return isJSPath(path) ? path.substr(0, path.length - 3) : path
-}
-function isModuleRequest(url) {
-  return 0 <= String(url.search).indexOf('module') && isJSPath(url.pathname)
-}
-
-function getModuleId(path, name) {
-  var id = getPackageRelativePath(path, name)
-  if (id == path) id = '.' + id
-  else id = removeJSExtension(id)
-  return id
-}
-function getContentPath(path) {
-  return String(path).substr(1)
-}
-
-function isTransportRequest(url) {
-  return 0 <= String(url.search).indexOf('transport')
-}
-
-function isRegistryRequest(url) {
-  return url === '/registry.json'
-}
-
-function getPackageContentForPath(pack, path, packageName) {
-  var content, name
-  if (isUnderPackages(path)) {
-    name = getPackageName(path)
-    if (packageName !== name) pack = pack.get('dependencies').get(name)
-    content = getPackageContentForPath(pack, getPackageRelativePath(path, name))
-  } else {
-    content = pack.invoke('getContent', [getContentPath(path)])
-  }
-  return content
-}
+var connect = require('connect')
+var path = require('path')
+var teleport = require('./app').teleport
 
 exports.activate = function activate() {
-  return when(getDescriptor().get("name"), start.bind(null),
-              start.bind(null, 'teleport-dashboard'))
-}
-
-
-function start(name) {
-  server.listen(4747)
-  console.log('Teleport is activated: http://localhost:4747/' + name)
-  server.on('request', function(request, response) {
-    var url = parseURL(request.url)
-      , needToWrap = isTransportRequest(url)
-      , path = url.pathname
-      , normalizedPath = normalizePath(path)
-      , relativePath
-      , packageName
-      , mime
-      , content
-      , pack
-
-    // If user has requested anything that is not under packages folder we
-    // can't handle that so we should redirect to a packages/rest/of/path
-    // instead.
-    if (isRegistryRequest(path))
-      content = registry.invoke('stringify', ['    '])
-    // If request path does not ends with '/' and it's not request to file
-    // we add trailing slash ourself, so that all browsers will generate to
-    // correct relative paths.
-    else if (normalizedPath !== path) redirectTo(normalizedPath, response)
-    // If request path is just '/' then we don't have any package so we
-    // redirect to default teleport-dashboard.
-    else if (path == '/') redirectTo('/teleport-dashboard/', response)
-    // Otherwise we detect package name and path of the file that is being
-    // requested from that package.
-    else {
-      packageName = getPackageName(path)
-      relativePath = getPackageRelativePath(compeletPath(path), packageName)
-      mime = mimeType(String(relativePath))
-
-      if (packageName && relativePath) {
-        pack = registry.get('packages').get(packageName)
-        if (isModuleRequest(url)) {
-          var method = needToWrap ? 'getModuleTransport' : 'getModuleSource'
-          content = pack.invoke(method, [getModuleId(relativePath)])
-        // Module 'teleport' is deprecated, all the request to in in old format
-        // are redirected to a new static file 'teleport/teleport.js'.
-        } else if (relativePath.substr(1) == deprecatedPath) {
-          redirectTo('/' + newTeleportPath, response)
-          console.log('Usage of `' + deprecatedPath + '` is deprecated, please update your html to refer to `' + newTeleportPath + '` instead.')
-        } else {
-          content = getPackageContentForPath(pack, relativePath, packageName)
-        }
-      } else {
-        response.writeHead(404)
-        Promised.sync(response.end).call(response, playground)
-      }
-    }
-
-    if (content) {
-      when
-      ( content
-      , function onDocument(content) {
-          response.writeHead(200, { 'Content-Type': mime })
-          response.end(content)
-        }
-      , function onFailed(error) {
-          response.writeHead(404)
-          Promised.sync(response.end).call(response, playground)
-        }
-      )
-    }
+  var path = process.cwd()
+  teleport(path, function onReady(error, teleport) {
+    if (error) return console.error(error);
+    connect(
+      connect.static(path),
+      connect.static(teleport.dashboard),
+      connect.router(teleport.router())
+    ).listen(4747)
   })
 }
